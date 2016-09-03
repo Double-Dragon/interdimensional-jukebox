3673032b20fb8da16c4ccf399c0037b98eda1868
controtie <dylan.ad.tran@gmail.com>
2016-09-03 15:27:50 -0700

    flesh autojukebox

--- contracts/AUTO_IDJB.sol
+++ contracts/AUTO_IDJB.sol
@@ -1,15 +1,17 @@
 // Jukebox.sol
 
-contract IDJB {
+contract AUTOIDJB {
 
   song[] public playlist;
   uint public currentSongIndex;
-  uint public lastSongChange;
-
+  uint256 public lastSongChange;
+  
   event playNext(string title, string author, string url);
+  event CurrentSongIndex(uint currentIndex, uint currentLength);
 
   function IDJB() {
     currentSongIndex = 0;
+    lastSongChange = now;
   }
 
   struct song {
@@ -17,28 +19,40 @@ contract IDJB {
     string author;
     string url;
     uint videoLength; // in milliseconds
-    uint dateAdded;
-    address adder; 
   }
 
   function addSong(string title, string author, string url, uint videoLength) {
-    if (playlist.length - currentSongIndex <= 5) throw;
-    playlist.push(song(title, author, url, videoLength, now, msg.sender));
+    cleanPlaylist();
+    if (playlist.length - currentSongIndex >= 5) throw;
+    // adding songs should increase cost wrt. to videoLength;
+    playlist.push(song(title, author, url, videoLength));
+  }
+
+  function getNextSongs() returns (string nextSong, string nextNextSong) {
+    cleanPlaylist();
+    if ((playlist.length - currentSongIndex) >= 1) {
+        nextSong = playlist[currentSongIndex].url;
+    } else {
+        nextSong = "no more songs!";
+    }
+    if ((playlist.length - currentSongIndex) >= 2) {
+        nextNextSong = playlist[currentSongIndex + 1].url;
+    } else {
+        nextNextSong = 'no more songs!';   
+    }
+    // playNext(playlist[currentSongIndex].title, playlist[currentSongIndex].author, playlist[currentSongIndex].url);
   }
 
   function cleanPlaylist() {
-    var currentTime = now;
-    for (var i = currentSongIndex; currentSongIndex < playlist.length; i++) {
+    uint256 currentTime = now;
+    for (uint i = currentSongIndex; currentSongIndex < playlist.length; i++) {
       if (playlist[i].videoLength + lastSongChange < currentTime) {
         lastSongChange += playlist[i].videoLength;
         currentSongIndex += 1;
+      } else {
+        break;
       }
     }
+    CurrentSongIndex(currentSongIndex, playlist.length);
   }
-
-  function playNextSong() onlyMasterController {
-    lastSongChange = now;
-    currentSongIndex += 1;
-    playNext(playlist[currentSongIndex].title, playlist[currentSongIndex].author, playlist[currentSongIndex].url);
-  }
-} 
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,4 @@
 module.exports = function(deployer) {
+  deployer.deploy(AUTOIDJB);
   deployer.deploy(IDJB);
 };
