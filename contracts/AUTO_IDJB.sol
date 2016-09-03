0e3e56921cbb30d36377e26461f7b3058f5500bf
The-Chanman <ecphylo@gmail.com>
2016-09-03 16:53:18 -0700

    fixing contract logic and complexity

--- contracts/AUTO_IDJB.sol
+++ contracts/AUTO_IDJB.sol
@@ -7,19 +7,25 @@ contract AUTOIDJB {
   uint public lastSongChange;
   uint numQueued;
   
-  event playNext(string title, string author, string url);
+  event playNext(string title, string author, string id);
   event playlistInfo(uint currentIndex, uint currentLength);
 
   struct song {
     string title;
     string author;
-    string url;
+    string id;
     uint videoLength; // in milliseconds
   }
     
   // adding songs should increase cost wrt. to videoLength;
-  modifier playlistNotFull {
+  modifier notFullPlaylist {
     if (playlist.length - currentSongIndex >= numQueued)
+        throw;
+    _
+  }
+  
+  modifier notEmptyPlaylist {
+    if (playlist.length - currentSongIndex <= 0)
         throw;
     _
   }
@@ -30,25 +36,28 @@ contract AUTOIDJB {
     numQueued = 5;
   }
 
-  function addSong (string title, string author, string url, uint videoLength) playlistNotFull {
+  function addSong (string title, string author, string id, uint videoLength) notFullPlaylist {
     cleanPlaylist();
-
-    playlist.push(song(title, author, url, videoLength));
+    playlist.push(song(title, author, id, videoLength));
   }
 
   function getNextSongs() returns (string nextSong, string nextNextSong) {
     cleanPlaylist();
     if ((playlist.length - currentSongIndex) >= 1) {
-        nextSong = playlist[currentSongIndex].url;
+        nextSong = playlist[currentSongIndex].id;
     } else {
         nextSong = "no more songs!";
     }
     if ((playlist.length - currentSongIndex) >= 2) {
-        nextNextSong = playlist[currentSongIndex + 1].url;
+        nextNextSong = playlist[currentSongIndex + 1].id;
     } else {
         nextNextSong = 'no more songs!';   
     }
-    // playNext(playlist[currentSongIndex].title, playlist[currentSongIndex].author, playlist[currentSongIndex].url);
+    // playNext(playlist[currentSongIndex].title, playlist[currentSongIndex].author, playlist[currentSongIndex].id);
+  }
+
+  function getQueuedSongs() notEmptyPlaylist returns (uint sIndex, uint eIndex) {
+
   }
 
   function cleanPlaylist() {
