c65a880f4f6ac0d4598fb72908ac7b2ce84d1315
controtie <dylan.ad.tran@gmail.com>
2016-09-03 18:13:37 -0700

    add videoPlayer to JS files

---
+++ app/javascripts/videoPlayer.js
@@ -0,0 +1,48 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'YmNlWf3Lvhw',
+    events: {
+      'onReady': onPlayerReady,
+      // 'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+  console.log('Player is Ready');
+  setTimeout(playNextSong, 6000);
+}
+
+var stop = false;
+var defaultSong = {
+  'videoId': "oFRl4YmVP4Q",
+  'startSeconds': 0,
+  'endSeconds': 60,
+  'suggestedQuality': 'large'
+}
+
+function playNextSong() {
+  console.log('playNextSong wrapper');
+  if (!playlist[0]) {
+    setTimeout(() => playNext(defaultSong), 12000);
+  } else {
+    setTimeout(() => playNext(defaultSong), 5000);
+  }
+}
+
+function playNext(videoObject) {
+  console.log('playNext');
+  player.loadVideoById(videoObject);
+  playlist.pop();
+  playNextSong();
+}
--- truffle.js
+++ truffle.js
@@ -2,7 +2,8 @@ module.exports = {
   build: {
     "index.html": "index.html",
     "app.js": [
-      "javascripts/app.js"
+      "javascripts/app.js",
+      "javascripts/videoPlayer.js"
     ],
     "app.css": [
       "stylesheets/app.css"
