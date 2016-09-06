cbe1936ec16c9bd3e983d03f28d2b779e74ef7be
controtie <dylan.ad.tran@gmail.com>
2016-09-05 22:44:50 -0700

    change event listener style from listening on AddVideo to listening for on NextSongs

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -7,15 +7,24 @@ var jukebox = AUTOIDJB.deployed();
 
 function addSong() {
   var url = document.getElementById("url").value;
-  var author = document.getElementById("author").value;
   var title = document.getElementById("title").value;
-  jukebox.addSong(title, author, url, {from: account}).then(function(msg) {
+  jukebox.addSong(title, url, {from: account}).then(function(msg) {
     console.log("Success! Transaction ID:", msg);
   })
   .catch(function(e) {
-    console.log('Failure! Encountered Error:');
+    console.log("Failure! Encountered Error:");
     console.log(e);
   });
+}
+
+function getNextSongs() {
+  jukebox.getNextSongs({from: account}).then(function(msg) {
+    console.log("Success! Transaction ID:", msg);
+  })
+  .catch(function(e) {
+    console.log("Failure! Encountered Error:");
+    console.log(e);
+  })
 }
 
 var playlist = [];
@@ -35,14 +44,25 @@ window.onload = function() {
     accounts = accs;
     account = accounts[0];
   });
-  var addedSongs = jukebox.AddSong({}, {from: "latest"});
-  addedSongs.watch(function(err, result) {
-  // TODO: write helper to convert array to object.
-  console.log('Heard an event!');
-  console.log(result);
-  var newPlaylist = playlist.slice(0);
-  // TODO: add error handling on bad result
-  newPlaylist.push(result.args.id);
-  playlist = newPlaylist;
-});
+
+  // var addedSongs = jukebox.AddSong({}, {from: "latest"});
+  // addedSongs.watch(function(err, result) {
+  //   console.log('Heard an event!');
+  //   console.log(result);
+  //   var newPlaylist = playlist.slice(0);
+  //   newPlaylist.push(result.args.id);
+  //   playlist = newPlaylist;
+  // });
+
+  // TODO: remove entries which return string "empty"
+  var nextSongs = jukebox.NextSongs({from: "latest"});
+  nextSongs.watch(function(err, result) {
+    console.log('Next Songs coming up!');
+    console.log(result);
+    var newPlaylist = playlist.slice(0);
+    // TODO: add error handling on bad result
+    newPlaylist.push(result.args.id1);
+    newPlaylist.push(result.args.id2);
+    playlist = newPlaylist;
+  })
 }
