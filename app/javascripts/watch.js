600793161705911a1b4e66f29763390a00d686bc
controtie <dylan.ad.tran@gmail.com>
2016-09-14 21:11:23 -0700

    add NextSongs event watcher to watch client

--- app/javascripts/watch.js
+++ app/javascripts/watch.js
@@ -1,7 +1,7 @@
 web3 = new Web3(new Web3.providers.HttpProvider("https://morden.infura.io/CspWpgT4vuC2gVNShfNQ:443"));
 
 // juxebox contract address goes here
-var jukebox = AUTOIDJB.at("0x6b02b2424d67d9e9533ba87ab73bb12b32f55834");
+var jukebox = AUTOIDJB.at("0x3bcc8a1a34c4da9308d98afb76b8fafde2d39066");
 
 var playlist = [];
 
@@ -10,11 +10,24 @@ window.onload = function() {
   addedSongs.watch(function(err, result) {
     console.log('Heard an event!');
     console.log(result);
-    if (result.args.id === "empty") {
-      return;
-    }
-    var newPlaylist = playlist.slice(0);
-    newPlaylist.push(result.args.id);
-    playlist = newPlaylist;
+    // if (result.args.id === "empty") {
+    //   return;
+    // }
+    // var newPlaylist = playlist.slice(0);
+    // newPlaylist.push(result.args.id);
+    // playlist = newPlaylist;
+  });
+
+  var nextSongs = jukebox.NextSongs({}, {from: "pending"});
+  nextSongs.watch(function(err, result) {
+    console.log('Next Songs coming up!');
+    console.log(result);
+    // var newPlaylist = playlist.slice(0);
+    // for (var each in result.args) {
+    //   if (playlist.indexOf(result.args[each]) === -1) {
+    //     playlist.push(result.args[each]);
+    //   }
+    // }
+    // playlist = newPlaylist;
   });
 }
