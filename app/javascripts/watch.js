d5972a41ba5fcd37d1f9708e1dcc57dafae5aa91
controtie <dylan.ad.tran@gmail.com>
2016-09-15 00:10:46 -0700

    revert to adding playlist songs via AddSong event

--- app/javascripts/watch.js
+++ app/javascripts/watch.js
@@ -1,26 +1,26 @@
 web3 = new Web3(new Web3.providers.HttpProvider("https://morden.infura.io/CspWpgT4vuC2gVNShfNQ:443"));
 
 // juxebox contract address goes here
-var jukebox = AUTOIDJB.at("0x3bcc8a1a34c4da9308d98afb76b8fafde2d39066");
+var jukebox = AUTOIDJB.at("0x86378e41ebe6be06ef83c6629723b533e2dd4a33");
 
 var playlist = [];
 
 window.onload = function() {
   var addedSongs = jukebox.AddSong({}, {from: "latest"});
   addedSongs.watch(function(err, result) {
-    console.log('Heard an event!');
+    console.log('Heard event: Add Song');
     console.log(result);
-    // if (result.args.id === "empty") {
-    //   return;
-    // }
-    // var newPlaylist = playlist.slice(0);
-    // newPlaylist.push(result.args.id);
-    // playlist = newPlaylist;
+    if (result.args.id === "empty") {
+      return;
+    }
+    var newPlaylist = playlist.slice(0);
+    newPlaylist.push(result.args.id);
+    playlist = newPlaylist;
   });
 
   var nextSongs = jukebox.NextSongs({}, {from: "pending"});
   nextSongs.watch(function(err, result) {
-    console.log('Next Songs coming up!');
+    console.log('Heard Event: NextSongs');
     console.log(result);
     // var newPlaylist = playlist.slice(0);
     // for (var each in result.args) {
